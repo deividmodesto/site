bffc998cc5bb4ab5eb2b5cbd924a7ce8d9d7ceb2
Reunidas | Deivid Modesto <deivid.modesto@reunidasdopontal.local>
2025-11-15 20:30:46 -0300

    Teste: mudei o texto do rodapé (agora com save!)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-6 mt-10">
       <div className="container mx-auto text-center">
-        <p>&copy; 2025 Grupo Reunidas. Todos os direitos reservados.</p>
+        <p>&copy; 2025 Grupo Reunidas. Todos os direitos reservados. TESTE</p>
       </div>
     </footer>
   );
